1c90bc236d0a3e220219b7e6259be47ec35f8d95
nurfianqodar <77nurfianqodar@gmail.com>
2025-07-18 19:35:37 +0700

    idk

--- migrations/20250716165922_init.up.sql
+++ migrations/20250716165922_init.up.sql
@@ -1,3 +1,6 @@
+-- ---------------------------------------------
+-- users table
+-- ---------------------------------------------
 CREATE TABLE IF NOT EXISTS users (
     id uuid NOT NULL,
     email varchar(255) NOT NULL,
@@ -8,19 +11,30 @@ CREATE TABLE IF NOT EXISTS users (
     CONSTRAINT pk_users PRIMARY KEY (id)
 );
 
-CREATE INDEX IF NOT EXISTS idx_users_email ON users (email);
+CREATE INDEX IF NOT EXISTS idx_users_email
+ON users (email);
 
-CREATE INDEX IF NOT EXISTS idx_users_deleted_at ON users (deleted_at);
+CREATE INDEX IF NOT EXISTS idx_users_deleted_at
+ON users (deleted_at);
 
-CREATE UNIQUE INDEX IF NOT EXISTS uidx_users_email_deleted_at ON users (email)
+CREATE UNIQUE INDEX IF NOT EXISTS uidx_users_email_deleted_at
+ON users (email)
 WHERE
 deleted_at IS NULL;
 
+
+-- ---------------------------------------------
+-- gender enum
+-- ---------------------------------------------
 CREATE TYPE gender AS ENUM (
     'm',
     'f'
 );
 
+
+-- ---------------------------------------------
+-- profiles table
+-- ---------------------------------------------
 CREATE TABLE IF NOT EXISTS profiles (
     id uuid NOT NULL,
     user_id uuid NOT NULL,
@@ -34,71 +48,157 @@ CREATE TABLE IF NOT EXISTS profiles (
     location varchar(255),
     created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT pk_profiles PRIMARY KEY (id),
-    CONSTRAINT uq_profiles_user_id UNIQUE (user_id),
-    CONSTRAINT fk_profiles_user_id FOREIGN KEY (user_id) REFERENCES users (
-        id
-    ) ON DELETE CASCADE
+
+    CONSTRAINT pk_profiles
+    PRIMARY KEY (id),
+
+    CONSTRAINT uq_profiles_user_id
+    UNIQUE (user_id),
+
+    CONSTRAINT fk_profiles_user_id
+    FOREIGN KEY (user_id)
+    REFERENCES users (id)
+    ON DELETE CASCADE
 );
 
-CREATE INDEX IF NOT EXISTS idx_profiles_user_id ON profiles (user_id);
+CREATE INDEX IF NOT EXISTS idx_profiles_user_id
+ON profiles (user_id);
 
+
+-- ---------------------------------------------
+-- roles table
+-- ---------------------------------------------
 CREATE TABLE IF NOT EXISTS roles (
     id uuid NOT NULL,
     name varchar(32) NOT NULL,
-    CONSTRAINT pk_roles PRIMARY KEY (id),
-    CONSTRAINT uq_roles_name UNIQUE (name)
+
+    CONSTRAINT pk_roles
+    PRIMARY KEY (id),
+
+    CONSTRAINT uq_roles_name
+    UNIQUE (name)
 );
 
+
+-- ---------------------------------------------
+-- user_roles table (relation: users - roles)
+-- ---------------------------------------------
 CREATE TABLE IF NOT EXISTS user_roles (
     user_id uuid NOT NULL,
     role_id uuid NOT NULL,
-    CONSTRAINT pk_user_roles PRIMARY KEY (user_id, role_id),
-    CONSTRAINT fk_user_roles_user_id FOREIGN KEY (user_id) REFERENCES users (
-        id
-    ) ON DELETE CASCADE,
-    CONSTRAINT fk_user_roles_role_id FOREIGN KEY (role_id) REFERENCES roles (
-        id
-    ) ON DELETE CASCADE
+
+    CONSTRAINT pk_user_roles
+    PRIMARY KEY (user_id, role_id),
+
+    CONSTRAINT fk_user_roles_user_id
+    FOREIGN KEY (user_id)
+    REFERENCES users (id)
+    ON DELETE CASCADE,
+
+    CONSTRAINT fk_user_roles_role_id
+    FOREIGN KEY (role_id)
+    REFERENCES roles (id)
+    ON DELETE CASCADE
 );
 
-CREATE INDEX IF NOT EXISTS idx_user_roles_user_id ON user_roles (user_id);
+CREATE INDEX IF NOT EXISTS idx_user_roles_user_id
+ON user_roles (user_id);
 
-CREATE INDEX IF NOT EXISTS idx_user_roles_role_id ON user_roles (role_id);
+CREATE INDEX IF NOT EXISTS idx_user_roles_role_id
+ON user_roles (role_id);
 
+
+-- ---------------------------------------------
+-- permissions table
+-- ---------------------------------------------
 CREATE TABLE IF NOT EXISTS permissions (
     id varchar(63) NOT NULL,
-    CONSTRAINT pk_permissions PRIMARY KEY (id)
+
+    CONSTRAINT pk_permissions
+    PRIMARY KEY (id)
 );
 
+
+-- ---------------------------------------------
+-- user_permissions table (relation: users - permissions)
+-- ---------------------------------------------
 CREATE TABLE IF NOT EXISTS user_permissions (
     user_id uuid NOT NULL,
     permission_id uuid NOT NULL,
-    CONSTRAINT pk_user_permissions PRIMARY KEY (user_id, permission_id),
-    CONSTRAINT fk_user_permissions_user_id FOREIGN KEY (
-        user_id
-    ) REFERENCES users (id),
-    CONSTRAINT fk_user_permissions_permission_id FOREIGN KEY (
-        permission_id
-    ) REFERENCES permissions (id)
+
+    CONSTRAINT pk_user_permissions
+    PRIMARY KEY (user_id, permission_id),
+
+    CONSTRAINT fk_user_permissions_user_id
+    FOREIGN KEY (user_id)
+    REFERENCES users (id)
+    ON DELETE CASCADE,
+
+    CONSTRAINT fk_user_permissions_permission_id
+    FOREIGN KEY (permission_id)
+    REFERENCES permissions (id)
+    ON DELETE CASCADE
 );
 
-CREATE INDEX IF NOT EXISTS idx_user_permissions_user_id ON user_permissions (
-    user_id
-);
+CREATE INDEX IF NOT EXISTS idx_user_permissions_user_id
+ON user_permissions (user_id);
 
+
+-- ---------------------------------------------
+-- role_permissions table (relation: roles - permissions)
+-- ---------------------------------------------
 CREATE TABLE IF NOT EXISTS role_permissions (
     role_id uuid NOT NULL,
     permission_id uuid NOT NULL,
-    CONSTRAINT pk_role_permissions PRIMARY KEY (role_id, permission_id),
-    CONSTRAINT fk_role_permissions_role_id FOREIGN KEY (
-        role_id
-    ) REFERENCES roles (id),
-    CONSTRAINT fk_role_permissions_permission_id FOREIGN KEY (
-        permission_id
-    ) REFERENCES permissions (id)
+
+    CONSTRAINT pk_role_permissions
+    PRIMARY KEY (role_id, permission_id),
+
+    CONSTRAINT fk_role_permissions_role_id
+    FOREIGN KEY (role_id)
+    REFERENCES roles (id)
+    ON DELETE CASCADE,
+
+    CONSTRAINT fk_role_permissions_permission_id
+    FOREIGN KEY (permission_id)
+    REFERENCES permissions (id)
+    ON DELETE CASCADE
 );
 
-CREATE INDEX IF NOT EXISTS idx_role_permissions_role_id ON role_permissions (
-    role_id
+CREATE INDEX IF NOT EXISTS idx_role_permissions_role_id
+ON role_permissions (role_id);
+
+
+-- ---------------------------------------------
+-- plants table
+-- ---------------------------------------------
+CREATE TABLE IF NOT EXISTS plants (
+    id uuid PRIMARY KEY,
+    scitentific_name varchar(127) NOT NULL,
+
+    CONSTRAINT uq_plants_scientific_name
+        UNIQUE (scitentific_name)
 );
+
+
+-- ---------------------------------------------
+-- pests table
+-- ---------------------------------------------
+CREATE TABLE IF NOT EXISTS pests (
+    id uuid PRIMARY KEY,
+    scitentific_name varchar(127) NOT NULL,
+
+    CONSTRAINT uq_pests_scientific_name
+        UNIQUE (scitentific_name)
+);
+
+
+CREATE TABLE IF NOT EXISTS image_classifications (
+	id uuid PRIMARY KEY,
+	plant_id uuid NOT NULL,
+	disease_id uuid NOT NULL,
+	hash varchar(255) NOT NULL,
+	categories varchar(255) NOT NULL
+)
+
+
